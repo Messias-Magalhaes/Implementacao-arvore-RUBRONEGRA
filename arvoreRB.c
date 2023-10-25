90cf343558354f5c06a5e56af8f74063f2481c83
Messias X. Magalhães <112675518+Messias-Magalhaes@users.noreply.github.com>
2023-10-24 22:39:13 -0300

    Update arvoreRB.c

--- arvoreRB.c
+++ arvoreRB.c
@@ -1,11 +1,39 @@
-No* buscaArvoreRN(No* raiz, int chave) {
+No* buscaRN(No* raiz, int chave) {
     if (raiz == NULL || raiz->info == chave) {
         return raiz;
     }
 
     if (chave < raiz->info) {
-        return buscaArvoreRN(raiz->esq, chave);
+        return buscaRN(raiz->esq, chave);
     } else {
-        return buscaArvoreRN(raiz->dir, chave);
+        return buscaRN(raiz->dir, chave);
     }
+}
+
+No* inserirRN(No* raiz, int chave) {
+    if (raiz == NULL) {
+        return criarNo(chave);
+    }
+
+    if (chave < raiz->info) {
+        raiz->esq = inserirRN(raiz->esq, chave);
+    } else if (chave > raiz->info) {
+        raiz->dir = inserirRN(raiz->dir, chave);
+    } else {
+        return raiz;
+    }
+
+    if (raiz->dir != NULL && raiz->esq != NULL) {
+        if (raiz->dir->cor == VERMELHO && raiz->esq->cor == PRETO) {
+            raiz = rotacionaEsquerda(raiz);
+        }
+        if (raiz->esq->cor == VERMELHO && raiz->esq->esq != NULL && raiz->esq->esq->cor == VERMELHO) {
+            raiz = rotacionaDireita(raiz);
+        }
+        if (raiz->esq->cor == VERMELHO && raiz->dir->cor == VERMELHO) {
+            inverterCores(raiz);
+        }
+    }
+
+    return raiz;
 }
